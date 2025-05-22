3db2e62db6dac918c6c6bc7293b48355e83d95de
elrefai99 <mohamed.mostafa0699@gmail.com>
2025-05-22 22:32:43 +0300

    fix: comment fiald code

--- src/components/DarkMode.vue
+++ src/components/DarkMode.vue
@@ -1,8 +1,8 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
 </script>
 
 <template>
   <button icon-btn i-carbon-sun dark:i-carbon-moon @click="toggleDark()" />
-</template>
+</template> -->
