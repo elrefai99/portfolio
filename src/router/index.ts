874a04ba32227f1932291f8f1e232f121225afda
elrefai99 <mohamed.mostafa0699@gmail.com>
2025-05-25 10:02:35 +0300

    feat: add NotFound component and update router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProjetcsView from '../views/ProjetcsView.vue'
 import ResumeView from '../views/ResumeView.vue'
+import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,11 +17,16 @@ const router = createRouter({
       name: 'projects',
       component: ProjetcsView
     },
-        {
+    {
       path: '/resume',
       name: 'resume',
       component: ResumeView
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound,
+    }
     // {
     //   path: '/about',
     //   name: 'about',
---
+++ src/views/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="not-found">
+    <h1 text-8xl>404</h1>
+    <p>The page you are looking for does not exist.</p>
+    <router-link to="/">Go back to Home</router-link>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  text-align: center;
+  font-family: Arial, sans-serif;
+  color: #e8e8e8;
+}
+</style>
