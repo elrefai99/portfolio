c718774cfd796ffdefb261045ee75b45233fc7c0
elrefai99 <mohamed.mostafa0699@gmail.com>
2025-08-25 15:36:16 +0300

    refactor: rehandle navbar that remove icont of projects, resume and packages

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,27 +1,53 @@
 <script setup lang="ts">
+import { ref } from "vue"
 
+const isOpen = ref(false)
+
+function toggleMenu() {
+  isOpen.value = !isOpen.value
+}
 </script>
 
 <template>
-  <div flex="~ gap2" text-3xl items-center px2 pt6 pb2 p3 mx-7xl>
-    <h1 text-4xl font-serif op50>
+  <nav class="flex items-center justify-between px-4 py-3 md:px-12">
+    <!-- Logo -->
+    <h1 class="text-3xl font-serif opacity-70">
       <router-link to="/">
-      <span font-bold italic>el</span>
+        <span class="font-bold italic">el</span>
       </router-link>
     </h1>
-    <div flex-auto  />
-    <router-link text-2xl font-serif op50 icon-btn i-codicon:project to="/projects" />
-    <router-link text-2xl font-serif op50 icon-btn i-carbon:align-box-top-center to="/resume" />
-    <router-link text-2xl font-serif op50 icon-btn i-carbon:package to="/packages" />
 
-    <div md:mx-6 />
-    <a hidden md:flex text-2xl font-serif op50 icon-btn i-carbon-logo-github href="https://github.com/elrefai99" target="_blank" title="GitHub" />
-    <a hidden md:flex text-2xl font-serif op50 icon-btn i-carbon-logo-x href="https://x.com/elrefai99" target="_blank" title="X.com" />
-    <a hidden md:flex text-2xl font-serif op50 icon-btn i-carbon-logo-linkedin href="https://www.linkedin.com/in/elrefai99/" target="_blank" title="LinkedIn" />
-    <a hidden md:flex text-2xl font-serif op50 icon-btn i-carbon-logo-instagram href="https://www.instagram.com/elrefai99/" target="_blank" title="LinkedIn" />
-    <a hidden md:flex text-2xl font-serif op50 icon-btn i-carbon-logo-facebook href="https://www.facebook.com/hamada.mostafa.18007" target="_blank" title="Facebook" />
-    <!-- <a icon-btn i-carbon-logo- href="https://github.com/antfu/what-time" target="_blank" title="GitHub" /> -->
-    <!-- <DarkMode /> -->
-  </div>
+    <!-- Desktop Menu -->
+    <div class="hidden md:flex items-center gap-6 text-2xl font-serif opacity-70">
+      <router-link to="/projects">Projects</router-link>
+      <router-link to="/resume">Resume</router-link>
+      <router-link to="/packages">Packages</router-link>
+
+      <a class="hidden md:flex text-2xl font-serif op100 icon-btn i-carbon-logo-github" href="https://github.com/elrefai99" target="_blank" title="GitHub" />
+      <a class="hidden md:flex text-2xl font-serif op100 icon-btn i-carbon-logo-x" href="https://x.com/elrefai99" target="_blank" title="X.com" />
+      <a class="hidden md:flex text-2xl font-serif op100 icon-btn i-carbon-logo-linkedin" href="https://www.linkedin.com/in/elrefai99/" target="_blank" title="LinkedIn" />
+      <a class="hidden md:flex text-2xl font-serif op100 icon-btn i-carbon-logo-instagram" href="https://www.instagram.com/elrefai99/" target="_blank" title="Instagram" />
+      <a class="hidden md:flex text-2xl font-serif op100 icon-btn i-carbon-logo-facebook" href="https://www.facebook.com/hamada.mostafa.18007" target="_blank" title="Facebook" />
+    </div>
+
+    <!-- Mobile Hamburger -->
+    <button @click="toggleMenu" class="md:hidden text-3xl">
+      <i :class="isOpen ? 'i-carbon-close' : 'i-carbon-menu'" />
+    </button>
+
+    <!-- Mobile Dropdown -->
+    <div v-if="isOpen" class="absolute top-16 left-0 w-full bg-white dark:bg-gray-900 shadow-md flex flex-col items-center gap-4 py-6 text-xl font-serif">
+      <router-link @click="toggleMenu" to="/projects">Projects</router-link>
+      <router-link @click="toggleMenu" to="/resume">Resume</router-link>
+      <router-link @click="toggleMenu" to="/packages">Packages</router-link>
+
+      <div class="flex gap-4 mt-4 text-2xl">
+        <a class="icon-btn i-carbon-logo-github" href="https://github.com/elrefai99" target="_blank" title="GitHub" />
+        <a class="icon-btn i-carbon-logo-x" href="https://x.com/elrefai99" target="_blank" title="X.com" />
+        <a class="icon-btn i-carbon-logo-linkedin" href="https://www.linkedin.com/in/elrefai99/" target="_blank" title="LinkedIn" />
+        <a class="icon-btn i-carbon-logo-instagram" href="https://www.instagram.com/elrefai99/" target="_blank" title="Instagram" />
+        <a class="icon-btn i-carbon-logo-facebook" href="https://www.facebook.com/hamada.mostafa.18007" target="_blank" title="Facebook" />
+      </div>
+    </div>
+  </nav>
 </template>
-
