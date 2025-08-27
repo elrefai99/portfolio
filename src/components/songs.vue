f0b5dfd9b61711ca3c2c270823ea35e4637cb528
elrefai99 <mohamed.mostafa0699@gmail.com>
2025-08-27 16:51:27 +0300

    feat: make roundom songs play in home page and playlist

--- src/components/songs.vue
+++ src/components/songs.vue
@@ -1,44 +1,30 @@
+<script setup lang="ts">
+import { spotifyPlaylist, spotifySongs } from "../utils/spotify";
+import { ref } from "vue"
+
+const randomPlaylist = ref(
+  spotifyPlaylist[Math.floor(Math.random() * spotifyPlaylist.length)]
+)
+
+const shuffledSongs = [...spotifySongs].sort(() => Math.random() - 0.5).slice(0, 2)
+</script>
+
 <template>
-        <section>
-      <h2 class="text-2xl font-bold mb-6 text-center">What I’m Listening To 🎧</h2>
-      <div class="max-w-3xl mx-auto grid md:grid-cols-2 gap-6">
-        
-        <!-- Left: Playlist -->
-        <div>
-          <iframe 
-            class="rounded-xl shadow-lg"
-            src="https://open.spotify.com/embed/playlist/1wNirTC24xxOGVDAKf9sXu?utm_source=generator" 
-            width="100%" 
-            height="352" 
-            frameBorder="0" 
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-            loading="lazy">
-          </iframe>
-        </div>
-
-        <!-- Right: Songs stacked -->
-        <div class="flex flex-col gap-4">
-          <iframe 
-            class="rounded-xl shadow-lg"
-            src="https://open.spotify.com/embed/track/5qGHMDEUWSn8vVRyIgGXTi?utm_source=generator&theme=0" 
-            width="100%" 
-            height="152" 
-            frameBorder="0" 
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-            loading="lazy">
-          </iframe>
-
-          <iframe 
-            class="rounded-xl shadow-lg"
-            src="https://open.spotify.com/embed/track/6IZF8hi9J6EtFDLQqOlSbu?utm_source=generator" 
-            width="100%" 
-            height="152" 
-            frameBorder="0" 
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-            loading="lazy">
-          </iframe>
-        </div>
-
+  <section>
+    <h2 class="text-2xl font-bold mb-6 text-center">What I’m Listening To 🎧</h2>
+    <div class="max-w-3xl mx-auto grid md:grid-cols-2 gap-6">
+      <div>
+        <iframe
+          v-bind="randomPlaylist"
+        ></iframe>
       </div>
-    </section>
+      <div class="flex flex-col gap-4">
+        <iframe
+          v-for="song in shuffledSongs"
+          :key="song.id"
+          v-bind="song"
+        ></iframe>
+      </div>
+    </div>
+  </section>
 </template>
