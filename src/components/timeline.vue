9b498a09b0c35d0e7b5d329ddca553ede655fc4f
elrefai99 <mohamed.mostafa0699@gmail.com>
2025-08-28 12:24:56 +0300

    chore: handle CSS of class to be UnoCSS and add some new songs

--- src/components/timeline.vue
+++ src/components/timeline.vue
@@ -8,7 +8,7 @@ const timeline = [
     color: "bg-blue-500"
   },
   {
-    company: "Modern Academy Computer Science",
+    company: "Modern Academy (CS)",
     role: "B.S. Computer Science",
     period: "2018 - 2022",
     desc: "got my degree to make my parents proud",
@@ -18,24 +18,24 @@ const timeline = [
     company: ".md",
     role: "Founder",
     period: "2019 - 2023",
-    desc: "built reselling community and developed tools",
+    desc: "a freelance with my friend in academy",
     color: "bg-red-500"
   }
 ]
 </script>
 <template>
-    <div class="max-w-xl mx-auto space-y-12 p-1 min-h-screen text-white flex justify-center items-start">
-  <div class="w-full max-w-4xl p-10">
+    <div max-w-xl mx-auto space-y-12 p-1 min-h-screen text-white flex justify-center items-start>
+  <div w-full max-w-4xl p-10>
     <section>
-      <h2 class="text-2xl font-bold mb-6 text-center">Timeline</h2>
-      <div class="border-l border-gray-700 pl-6 relative">
-        <div v-for="(item, i) in timeline" :key="i" class="mb-8 relative">
+      <h2 text-2xl font-bold mb-6 text-center>Timeline</h2>
+      <div border-l border-gray-700 pl-6 relative>
+        <div v-for="(item, i) in timeline" :key="i" class="" mb-8 relative>
           <span :class="['absolute -left-3 -ml-5.5 w-5 h-5 rounded-full', item.color]"></span>
           <div>
-            <h3 class="font-semibold">{{ item.company }}</h3>
-            <p class="italic text-gray-400">{{ item.role }}</p>
-            <p class="text-sm text-gray-300">{{ item.desc }}</p>
-            <span class="absolute right-0 top-0 text-sm text-gray-500">{{ item.period }}</span>
+            <h3 font-semibold>{{ item.company }}</h3>
+            <p italic text-gray-400>{{ item.role }}</p>
+            <p text-sm text-gray-300>{{ item.desc }}</p>
+            <span absolute right-0 top-0 text-sm text-gray-500>{{ item.period }}</span>
           </div>
         </div>
       </div>
