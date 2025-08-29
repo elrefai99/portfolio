f5a9dc3aa1d7dae8d23c98643cb7a261ac3dfe4f
elrefai99 <mohamed.mostafa0699@gmail.com>
2025-08-29 20:21:04 +0300

    chore: adding line in nav bar that current page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -15,44 +15,33 @@ const isOpen = ref(false)
 </style>
 
 <template>
-  <nav class="w-full bg-transparent">
-    <div class="max-w-xl mx-auto flex items-center justify-between px-6 py-4">
+  <nav w-full bg-transparent>
+    <div max-w-xl mx-auto flex items-center justify-between px-6 py-4>
       <!-- Logo -->
-      <h1 class="text-xl font-serif opacity-80">
-        <router-link to="/" class="flex items-center gap-1">
-          <span class="font-bold italic">el</span>
+      <h2 text-4xl font-serif opacity-80>
+        <router-link to="/" flex items-center gap-1>
+          <span font-bold italic>el</span>
         </router-link>
-      </h1>
-
-      <!-- Mobile Hamburger -->
-      <button 
-        class="md:hidden flex flex-col gap-1.5"
-        @click="isOpen = !isOpen"
-        aria-label="Toggle Menu"
-      >
-        <span class="w-6 h-0.5 bg-current"></span>
-        <span class="w-6 h-0.5 bg-current"></span>
-        <span class="w-6 h-0.5 bg-current"></span>
-      </button>
+      </h2>
 
       <!-- Desktop Menu -->
-      <div class="hidden md:flex items-center gap-8 opacity-80">
-        <router-link to="/" class="hover:text-white-400 transition">Home</router-link>
-        <!-- <router-link to="/about" class="hover:text-white-400 transition">About</router-link> -->
-        <router-link to="/projects" class="hover:text-white-400 transition">Projects</router-link>
-        <a class="hover:text-white-400 transition" href="/resume.pdf">Resume</a>
+      <div text-xl hidden md:flex items-center gap-8 opacity-80>
+        <router-link to="/" hover:text-white-400 transition active-class="border-b-2 border-current pb-1">Home</router-link>
+        <router-link to="/projects" hover:text-white-400 transition active-class="border-b-2 border-current pb-1">Projects</router-link>
+        <a hover:text-white-400 transition href="/resume.pdf">Resume</a>
       </div>
+
+      <!-- Mobile Hamburger -->
+      <button md:hidden flex flex-col gap-1.5 @click="isOpen = !isOpen"aria-label="Toggle Menu">
+        <span i-carbon:menu text-3xl></span>
+      </button>
     </div>
 
     <!-- Mobile Dropdown -->
-    <div 
-      v-if="isOpen"
-      class="md:hidden flex flex-col gap-4 px-6 pb-4 animate-fadeIn opacity-90"
-    >
-      <router-link @click="isOpen=false" to="/" class="hover:text-white-400 transition">Home</router-link>
-      <!-- <router-link @click="isOpen=false" to="/about" class="hover:text-white-400 transition">About</router-link> -->
-      <router-link @click="isOpen=false" to="/projects" class="hover:text-white-400 transition">Projects</router-link>
-      <a @click="isOpen=false" class="hover:text-white-400 transition" href="/resume.pdf">Resume</a>
+    <div v-if="isOpen" text-center md:hidden flex flex-col gap-4 px-6 pb-4 opacity-90 class="animate-fadeIn">
+      <router-link @click="isOpen=false" to="/" hover:text-white-400 transition active-class="border-b-2 border-current pb-1">Home</router-link>
+      <router-link @click="isOpen=false" to="/projects" hover:text-white-400 transition active-class="border-b-2 border-current pb-1">Projects</router-link>
+      <a @click="isOpen=false" hover:text-white-400 transition href="/resume.pdf">Resume</a>
     </div>
   </nav>
 </template>
