75963088102e607c117ca204a6d853c70509bf9a
elrefai99 <mohamed.mostafa0699@gmail.com>
2025-09-02 13:54:40 +0300

    chore: make min branch 10

--- src/components/BackGround.vue
+++ src/components/BackGround.vue
@@ -39,7 +39,7 @@ function createEffect(effectType: number) {
     let steps: (() => void)[] = []
     let prevSteps: (() => void)[] = []
     const len: number = 6
-    const MIN_BRANCH: number = 15
+    const MIN_BRANCH: number = 10
 
     const step = (
       x: number, y: number, rad: number,
