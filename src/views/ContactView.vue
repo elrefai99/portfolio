b59bae8a9febfd0bee4b0d850a6201ed522dc81c
elrefai99 <mohamed.mostafa0699@gmail.com>
2025-10-16 10:54:04 +0300

    refactor: remove unused CSS, logo, and contact view components; update project links styles for consistency

--- src/views/ContactView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-        <div max-w-2xl mx-auto space-y-12 min-h-screen text-white flex justify-center items-start>
-        <div w-full max-w-4xl p-10>
-        </div>
-    </div>
-</template>
